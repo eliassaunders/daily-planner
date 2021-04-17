388c5ed5fbdc5b2113d6b11a90a086eb7248d71a
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-17 11:13:31 -0700

    added base design for website

---
+++ Assets/JS/script.js
@@ -0,0 +1,26 @@
+var container = $("#time-array");
+
+var timeInputs6a = $("#input-field6a");
+var timeInputs7a = $("#input-field7a");
+var timeInputs8a = $("#input-field8a");
+var timeInputs9a = $("#input-field9a");
+var timeInputs10a = $("#input-field10a");
+var timeInputs11a = $("#input-field11a");
+var timeInputs12p = $("#input-field12p");
+var timeInputs1p = $("#input-field1p");
+var timeInputs2p = $("#input-field2p");
+var timeInputs3p = $("#input-field3p");
+
+var buttonInput1 = $('.saveImage2')
+
+
+var inputArray = [timeInputs6a, timeInputs7a, timeInputs8a, timeInputs9a, timeInputs10a, timeInputs11a]
+var saveImage = $(".saveImage")
+
+var saveInputData = function(event) {
+    var thisSave = 
+    
+    if (time)
+}
+
+saveImage.addEventListener("click", saveInputData)
